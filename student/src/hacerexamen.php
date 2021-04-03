4594ae93c218b423f70f106d19bc3f4dcc3d30ef
alfredoor <50826496+alfredoor@users.noreply.github.com>
2021-04-03 12:49:08 +0200

    Update hacerexamen.php

--- student/src/hacerexamen.php
+++ student/src/hacerexamen.php
@@ -6,54 +6,34 @@ session_start();
 ?>
 
 <html>
-<h1 style="column-span: all; text-align: center">Lista de examenes de <?php echo $nombresesion." ".$apellsesion ?></h1>
+<h1 style="column-span: all; text-align: center">Examen de PW</h1>
 </html>
-
 <?php 
-
-include('panelexamen.php');
-
-	print $_POST['fecha'];
-    $conexion = mysqli_connect('localhost','root', '', 'universidad');
+			$conexion = mysqli_connect('localhost','root', '', 'universidad');
 	
-	$consulta = mysqli_query($conexion,"SELECT examenes.fecha, examenes.tema, examenes.asignatura FROM persona,alumno,asignaturas,temas,examenes WHERE persona.nombre='$nombresesion' && persona.apellidos='$apellsesion' && persona.dni = alumno.dni && alumno.curso=asignaturas.curso && asignaturas.id=temas.id_asig && asignaturas.nombre= examenes.asignatura && temas.num_tema=examenes.tema");
-    $numexam = mysqli_num_rows($consulta);
+			$consult = mysqli_query($conexion,"SELECT * FROM preguntas WHERE preguntas.tema=1 && preguntas.asignatura='PW'");
 	
+			for ($i=0; $i<10; $i++){
+				$fil = mysqli_fetch_array($consult);
+				
 	
-	
-	for ($i=0; $i<$numexam; $i++){
-		$fila = mysqli_fetch_array($consulta);
-		if($fila['asignatura']=='PW'){
-			$asig = $fila['asignatura'];
-			$tema = $fila['tema'];
-			
-		}
-	}
-		
-	
-		$consulta = mysqli_query($conexion,"SELECT * from preguntas where preguntas.tema=$tema && preguntas.asignaturas=$asig");
-		
-		for ($i=0; $i<10; $i++){
-		$fila = mysqli_fetch_array($consulta);
-		?>
+	?>
 		<html>
-		<form mthod='post' action='hacerexamen.php'>
-			<?php echo $fila['enunciado']; ?>
-			<input name='respuesta' type='radio' value='resp1' /> <?php echo $fila["resp1"]; ?>
-			<input name='respuesta' type='radio' value='resp2' /> <?php echo $fila["resp2"]; ?>
-			<input name='respuesta' type='radio' value='resp3' /> <?php echo $fila["resp3"]; ?>
-			<input name='respuesta' type='radio' value='resp4' /> <?php echo $fila["resp4"]; ?>
+		<form>
+			<?php print $fil['enunciado']; ?>
+
+			<input name='respuesta' type='radio' value='resp1' /> <?php print $fil['resp1']; ?>
+			<input name='respuesta' type='radio' value='resp2' /> <?php print $fil['resp2']; ?>
+			<input name='respuesta' type='radio' value='resp3' /> <?php print $fil['resp3']; ?>
+			<input name='respuesta' type='radio' value='resp4' /> <?php print $fil['resp4']; ?>
 			
 			<input type='submit' value='Enviar'>
+					
+			
 		</form>
 		
 		</html>
-		<?php
-	}
-    
-		
-	
-?>   
+<?php }	?>	
     
 
 
