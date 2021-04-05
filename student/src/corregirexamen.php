0ff7444dfa487fd172ffae9f5ad6bb1f568ef0a9
alfredoor <50826496+alfredoor@users.noreply.github.com>
2021-04-05 19:30:29 +0200

    Update corregirexamen.php

--- student/src/corregirexamen.php
+++ student/src/corregirexamen.php
@@ -1,5 +1,32 @@
 <?php
-
-	print $_POST['respuesta1'];
-
+session_start();
+	$nombresesion = $_SESSION['nombre'];
+    $apellsesion = $_SESSION['apellidos'];
+	$tema = $_SESSION ['tema'];
+	$asignatura = $_SESSION ['asignatura'];
+    
 ?>
+
+<?php 
+
+	$conexion = mysqli_connect('localhost','root', '', 'universidad');
+	
+	$consult = mysqli_query($conexion,"SELECT * FROM preguntas WHERE preguntas.tema='$tema' && preguntas.asignatura='$asignatura'");
+	
+	$nota=0;
+	
+	for ($i=1; $i<=10; $i++){
+		$fil = mysqli_fetch_array($consult);
+		
+		if ($_POST['respuesta1'] == $fil['CORRECTA'])
+			$nota++;		
+		
+		}
+				
+	print "La nota final del examen es ".$nota."/10";
+
+
+
+
+
+?> 
